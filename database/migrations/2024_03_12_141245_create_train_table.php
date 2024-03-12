049eb9ce651ffce4613b2b340bb4978456608260
mariocstweb <mariocstmra@gmail.com>
2024-03-12 15:38:02 +0100

    added property in schema

--- database/migrations/2024_03_12_141245_create_train_table.php
+++ database/migrations/2024_03_12_141245_create_train_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('train', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 20);
+            $table->string('departure_station', 30);
+            $table->string('arrival_station', 30);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('cp', 5)->unique();
+            $table->tinyInteger('number_of_carriages');
+            $table->boolean('is_intime')->default(1);
+            $table->boolean('deleted')->default(0);
             $table->timestamps();
         });
     }
